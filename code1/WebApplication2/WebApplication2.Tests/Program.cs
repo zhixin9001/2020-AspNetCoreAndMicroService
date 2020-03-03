41a7688353e98a8ddefc70e2615fcbf8b3109237
dante.x.zhi <dante.x.zhi@newegg.com>
2020-03-03 12:33:50 +0800

    3.2

---
+++ code1/WebApplication2/WebApplication2.Tests/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xunit;
+
+namespace WebApplication2.Tests
+{
+    public class Program
+    {
+        [Fact]
+        public void Test()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

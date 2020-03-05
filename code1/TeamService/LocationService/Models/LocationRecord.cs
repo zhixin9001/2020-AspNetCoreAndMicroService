4ca1e3f6d044c61e427bce2b6be50fa3a23207df
zhixin9001 <zhixin9001@live.com>
2020-03-05 21:50:48 +0800

    3.5

---
+++ code1/TeamService/LocationService/Models/LocationRecord.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LocationService.Models
+{
+    public class LocationRecord
+    {
+        public Guid ID { get; set; }
+        public float Latitude { get; set; }
+        public float Longitude { get; set; }
+        public float Altitude { get; set; }
+        public long Timestamp { get; set; }
+        public Guid MemberID { get; set; }
+    }
+}

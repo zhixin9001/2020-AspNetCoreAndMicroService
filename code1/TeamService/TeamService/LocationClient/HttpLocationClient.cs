393a7b74319f2125af671f5fed0e98f626ab9345
zhixin9001 <zhixin9001@live.com>
2020-03-06 07:01:35 +0800

    3.6

---
+++ code1/TeamService/TeamService/LocationClient/HttpLocationClient.cs
@@ -0,0 +1,66 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using TeamService.Entities;
+
+namespace TeamService.LocationClient
+{
+    public class HttpLocationClient : ILocationClient
+    {
+        public String URL { get; set; }
+
+        public HttpLocationClient(string url)
+        {
+            this.URL = url;
+        }
+
+        public async Task<LocationRecord> GetLatestForMember(Guid memberId)
+        {
+            LocationRecord locationRecord = null;
+
+            using (var httpClient = new HttpClient())
+            {
+                httpClient.BaseAddress = new Uri(this.URL);
+                httpClient.DefaultRequestHeaders.Accept.Clear();
+                httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpResponseMessage response = await httpClient.GetAsync(String.Format("/locations/{0}/latest", memberId));
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string json = await response.Content.ReadAsStringAsync();
+                    locationRecord = JsonConvert.DeserializeObject<LocationRecord>(json);
+                }
+            }
+
+            return locationRecord;
+        }
+
+        public async Task<LocationRecord> AddLocation(Guid memberId, LocationRecord locationRecord)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                httpClient.BaseAddress = new Uri(this.URL);
+                httpClient.DefaultRequestHeaders.Accept.Clear();
+                httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var jsonString = JsonConvert.SerializeObject(locationRecord);
+                var uri = String.Format("/locations/{0}", memberId);
+                HttpResponseMessage response =
+                  await httpClient.PostAsync(uri, new StringContent(jsonString, Encoding.UTF8, "application/json"));
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string json = await response.Content.ReadAsStringAsync();
+                }
+            }
+
+            return locationRecord;
+        }
+    }
+}
---
+++ code1/TeamService/TeamService/LocationClient/MemoryLocationClient.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TeamService.Entities;
+
+namespace TeamService.LocationClient
+{
+    public class MemoryLocationClient : ILocationClient
+    {
+        public Dictionary<Guid, SortedList<long, LocationRecord>> MemberLocationHistory { get; set; }
+
+        public MemoryLocationClient()
+        {
+            this.MemberLocationHistory = new Dictionary<Guid, SortedList<long, LocationRecord>>();
+        }
+
+        public async Task<LocationRecord> AddLocation(Guid memberId, LocationRecord locationRecord)
+        {
+            return await Task.Run(() =>
+            {
+                if (!MemberLocationHistory.ContainsKey(memberId))
+                {
+                    MemberLocationHistory.Add(memberId, new SortedList<long, LocationRecord>());
+                }
+
+                MemberLocationHistory[memberId].Add(locationRecord.Timestamp, locationRecord);
+
+                return locationRecord;
+            });
+        }
+
+        public async Task<LocationRecord> GetLatestForMember(Guid memberId)
+        {
+            return await Task.Run(() =>
+            {
+                if (MemberLocationHistory.ContainsKey(memberId))
+                {
+                    return MemberLocationHistory[memberId].Values.LastOrDefault();
+                }
+
+                return null;
+            });
+        }
+    }
+}
